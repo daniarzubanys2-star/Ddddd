24e570af236232871732c67493d51969ef010de8
daniarzubanys2-star <daniarzubanys2@gmail.com>
2026-01-06 12:14:52 +0500

    Doctor

--- Doctor.java
+++ Doctor.java
@@ -1,71 +1,29 @@
-public class Doctor {
-
-
-    private String name;
+public class Doctor extends Person {
     private String specialization;
-    private String ageGroup;
-    private String gender;
     private int experienceYears;
 
-
-    public Doctor() {
-    }
-
-
-    public Doctor(String name, String specialization, String ageGroup, String gender, int experienceYears) {
-        this.name = name;
-        this.specialization = specialization;
-        this.ageGroup = ageGroup;
-        this.gender = gender;
-        this.experienceYears = experienceYears;
-    }
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSpecialization() {
-        return specialization;
+    public Doctor(String name, int age, String specialization, int experienceYears) {
+        super(name, age);
+        setSpecialization(specialization);
+        setExperienceYears(experienceYears);
     }
 
     public void setSpecialization(String specialization) {
-        this.specialization = specialization;
+        if (specialization == null || specialization.isEmpty()) {
+            this.specialization = "General";
+        } else {
+            this.specialization = specialization;
+        }
     }
 
-    public String getAgeGroup() {
-        return ageGroup;
-    }
-
-    public void setAgeGroup(String ageGroup) {
-        this.ageGroup = ageGroup;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public int getExperienceYears() {
-        return experienceYears;
-    }
 
     public void setExperienceYears(int experienceYears) {
-        this.experienceYears = experienceYears;
+        if (experienceYears < 0) {
+            this.experienceYears = 0;
+        } else {
+            this.experienceYears = experienceYears;
+        }
     }
-
-
-    public boolean worksWithAgeGroup(String group) {
-        return ageGroup.equals(group);
-    }
-
 
     public boolean isExperienced() {
         return experienceYears >= 5;
@@ -73,12 +31,19 @@ public class Doctor {
 
 
     @Override
+    public String getRole() {
+        return "Doctor";
+    }
+
+    @Override
     public String toString() {
-        return "Doctor{name='" + name + "', gender='" + gender +
-                "', specialization='" + specialization +
-                "', ageGroup='" + ageGroup +
-                "', experienceYears=" + experienceYears + "}";
+        return super.toString() +
+                ", Role: Doctor" +
+                ", Specialization: " + specialization +
+                ", Experience: " + experienceYears + " years";
     }
 }
+
+
 
 
